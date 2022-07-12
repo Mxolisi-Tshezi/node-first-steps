e5bca8df55b634e7ed1d94c2ca0d672fac59132c
Mxolisi <nyawose57@gmail.com>
2022-07-12 16:29:36 +0200

    added figlet

--- greet_xola.js
+++ greet_xola.js
@@ -1,5 +1,14 @@
 import chalk from 'chalk';
 import greet from './greet.js'
+import figlet from 'figlet';
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+
+figlet('Hello Xola!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    const styledMessage = chalk.bgGreen.black(greet('Xola!'));
+    console.log(styledMessage)    
+});
